c4d97669d9b0233fa94cc8895b641d3c9f83e794
SyedSami07 <syedsamigithub.com>
2026-09-10 00:27:34 +0600

    initialized the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2>Nusu and Sami on the go...</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/User.tsx
@@ -0,0 +1,8 @@
+export interface UserProps {
+    prop: string
+}
+
+export default function User({ prop }: UserProps) {
+    
+    return 
+}
