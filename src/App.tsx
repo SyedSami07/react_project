93bfba2fb693407b240aa25a2d13879c08470d64
SyedSami07 <syedsamigithub.com>
2026-09-10 00:44:06 +0600

    explore API and load data using USE

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Suspense } from 'react'
 import './App.css'
+import type { CountryType } from './type'
+import Countries from './components/Countries'
+
+// step 1: create a promise to load data
+const countriesPromise = async():Promise<CountryType[]> => {
+  const res = await fetch('https://openapi.programming-hero.com/api/all')
+  const data = await res.json()
+  return data.countries
+}
 
 function App() {
 
   return (
     <>
-     <h2>Nusu and Sami on the go...</h2>
+     <h2>N and S on the go...</h2>
+     <Suspense fallback={<div>SN Loading...</div>}>
+      <Countries countriesPromise={countriesPromise()}></Countries>
+     </Suspense>
     </>
   )
 }
